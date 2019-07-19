458d80b390476b5fa71598fbd35a0dc12c392133
Roger Veciana i Rovira <rveciana@gmail.com>
2019-07-19 23:53:32 +0200

    Resolves animated gifs and cleans unused files

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Fun with flags!
 I wanted to learn some react hooks using redux and redux-observable, and this is the result.
 
 Check the game at geoexamples.com/fun-with-flags
+
+![alt text](fun-with-flags.gif "Fun with flags!")
--- src/index.js
+++ src/index.js
@@ -4,13 +4,8 @@ import './index.css';
 import App from './App';
 import store from './store'
 import { Provider } from 'react-redux'
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Provider store={store}>
     <App />
   </Provider>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
